9ccf62bd420e3832330b2c3978efeef38a935753
kae03 <kae02065@gmail.com>
2023-02-24 00:34:44 -0500

    initial commit

---
+++ boostrapdjango/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+def index (request) :
+    return render(request, 'index.html')
+def about(request):
+    return render(request, 'about.html')
+def contact(request):
+    return render(request, 'contact.html ')
+def house (request):
+    return render(request, 'house.html')
+def price(request):
+    return render(request, 'price.html')
